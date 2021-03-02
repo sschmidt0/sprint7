8c8c8d3a5cfb4e1d16d3348367c4dcab482aa57e
sarah <sschmidt0@uoc.edu>
2021-03-02 18:53:00 +0100

    Nivell 3 - exercici 3

--- src/App.js
+++ src/App.js
@@ -1,10 +1,37 @@
+import { useState } from 'react';
 import Línia from './components/Línia';
+import styled from 'styled-components';
+import textos from './assets/textos';
+
+const DivContainer = styled.div`
+  width: 400px;
+  margin: 80px auto 30px;
+`;
+
+const Button = styled.button`
+  width: 200px;
+  padding: 15px;
+  background-color: lightblue;
+  text-transform: uppercase;
+  border: 1px solid #333;
+`;
+
+
 
 function App() {
+  const [text, setText] = useState(0);
+
+  const lastItem = () => setText(prevText => prevText < 1 ? (prevText = textos.length-1) : prevText - 1);
+  const nextItem = () => setText(prevText => prevText > textos.length-2 ? (prevText = 0) : prevText + 1);
+
   return (
-    <div className="App">
-      <Línia />
-    </div>
+    <DivContainer className="App">
+      <div>
+        <Button onClick={ () => lastItem() }>Anterior</Button>
+        <Button onClick={ () => nextItem() }>Següent</Button>
+      </div>
+      <Línia text={textos[text]}/>
+    </DivContainer>
   );
 }
 
--- src/components/Línia.js
+++ src/components/Línia.js
@@ -1,30 +1,23 @@
-import textos from '../assets/textos';
 import styled from 'styled-components';
 
-const Línia = () => {
+const DivTextos = styled.div`
+  padding: 30px 50px;
+  color: #fff;
+  background-color: darkred;
+  border-radius: 0 0 15px 15px;
+`;
 
-  const Div = styled.div`
-    width: 400px;
-    margin: 70px auto 30px;
-    padding: 30px 50px;
-    color: #fff;
-    background-color: darkred;
-    border-radius: 15px;
-  `;
+const Paragraph = styled.p`
+  font-family: 'Papyrus', 'Times New Roman';
+  font-weight: 800;
+  letter-spacing: 1.5px;
+`;
 
-  const Paragraph = styled.p`
-    font-family: 'Papyrus', 'Times New Roman';
-    font-weight: 800;
-    letter-spacing: 1.5px;
-  `;
-
-
-  const line = textos.map(text => {
-    return <Paragraph>{ text }</Paragraph>;
-  });
-
+const Línia = (props) => {
   return (
-    <Div>{ line }</Div>
+    <DivTextos>
+      <Paragraph>{ props.text }</Paragraph>
+    </DivTextos>
   );
 }
 

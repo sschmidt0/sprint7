570f53076b2c5fd92a569099a6c94cf23f6b4505
sarah <sschmidt0@uoc.edu>
2021-03-02 17:49:59 +0100

    Nivell 2 - exercici 2

--- src/components/Línia.js
+++ src/components/Línia.js
@@ -1,12 +1,30 @@
 import textos from '../assets/textos';
+import styled from 'styled-components';
 
 const Línia = () => {
+
+  const Div = styled.div`
+    width: 400px;
+    margin: 70px auto 30px;
+    padding: 30px 50px;
+    color: #fff;
+    background-color: darkred;
+    border-radius: 15px;
+  `;
+
+  const Paragraph = styled.p`
+    font-family: 'Papyrus', 'Times New Roman';
+    font-weight: 800;
+    letter-spacing: 1.5px;
+  `;
+
+
   const line = textos.map(text => {
-    return <p>{ text }</p>;
+    return <Paragraph>{ text }</Paragraph>;
   });
 
   return (
-    <div>{ line }</div>
+    <Div>{ line }</Div>
   );
 }
 

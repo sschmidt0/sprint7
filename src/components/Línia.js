8d59d3db5f3f35addaf6468aa5188aaf8c6c35f0
sarah <sschmidt0@uoc.edu>
2021-03-03 15:36:48 +0100

    Exercici 2: added key in text list and created const Div and const Paragraph outside of Línia, already corrected in exercises 3 and 4

--- src/components/Línia.js
+++ src/components/Línia.js
@@ -1,26 +1,27 @@
 import textos from '../assets/textos';
 import styled from 'styled-components';
 
+
+const Div = styled.div`
+  width: 400px;
+  margin: 70px auto 30px;
+  padding: 30px 50px;
+  color: #fff;
+  background-color: darkred;
+  border-radius: 15px;
+`;
+
+const Paragraph = styled.p`
+  font-family: 'Papyrus', 'Times New Roman';
+  font-weight: 800;
+  letter-spacing: 1.5px;
+`;
+
 const Línia = () => {
-
-  const Div = styled.div`
-    width: 400px;
-    margin: 70px auto 30px;
-    padding: 30px 50px;
-    color: #fff;
-    background-color: darkred;
-    border-radius: 15px;
-  `;
-
-  const Paragraph = styled.p`
-    font-family: 'Papyrus', 'Times New Roman';
-    font-weight: 800;
-    letter-spacing: 1.5px;
-  `;
-
-
+  let key = 0;
   const line = textos.map(text => {
-    return <Paragraph>{ text }</Paragraph>;
+    key++;
+    return <Paragraph key={ key }>{ text }</Paragraph>;
   });
 
   return (

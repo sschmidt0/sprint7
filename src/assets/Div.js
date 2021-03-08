6ab444951ee63d7a577af2c8df19d0b67da5921d
sarah <sschmidt0@uoc.edu>
2021-03-08 15:42:57 +0100

    separated styled components

---
+++ src/assets/Div.js
@@ -0,0 +1,8 @@
+export default Div = styled.div`
+  width: 400px;
+  margin: 70px auto 30px;
+  padding: 30px 50px;
+  color: #fff;
+  background-color: darkred;
+  border-radius: 15px;
+`;
---
+++ src/assets/Paragraph.js
@@ -0,0 +1,5 @@
+export default Paragraph = styled.p`
+  font-family: 'Papyrus', 'Times New Roman';
+  font-weight: 800;
+  letter-spacing: 1.5px;
+`;

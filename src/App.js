ad86e6c6127652a5df2bf436a26fc5d705a9636e
sarah <sschmidt0@uoc.edu>
2021-03-08 16:17:39 +0100

    changed App.js according to suggestions

--- src/App.js
+++ src/App.js
@@ -1,11 +1,7 @@
 import { Linia } from './components/Linia';
 
-function App() {
-  return (
-    <div className="App">
-      <Linia />
-    </div>
-  );
-}
-
-export default App;
+export default () => (
+  <div className="App">
+    <Linia />
+  </div>
+);
